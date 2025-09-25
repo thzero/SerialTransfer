a47ebd56dd8d6809ae1d6468f7bc9f1ef9ce2e00
Corvette Rox <development@thzero.com>
2025-09-24 21:27:43 -0500

    cleanup

--- src/Packet.h
+++ src/Packet.h
@@ -32,8 +32,6 @@ const uint8_t STOP_BYTE  = 0x81;
 
 const uint8_t PREAMBLE_SIZE   = 5;
 const uint8_t POSTAMBLE_SIZE  = 3;
-// const uint8_t MAX_PACKET_SIZE = 0xFE; // Maximum allowed payload bytes per packet
-// const uint8_t MAX_PACKET_SIZE = 0x3F8; // 1024 - 8 premable/postamble size; Maximum allowed payload bytes per packet
 const uint16_t MAX_PACKET_SIZE = 0x400 - (uint16_t)PREAMBLE_SIZE - (uint16_t)POSTAMBLE_SIZE; // Maximum allowed payload bytes per packet
 
 const uint8_t DEFAULT_TIMEOUT = 50;
